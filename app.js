fc372624bbf86d60d492e5f9576a6ef3b25818c0
K Narayan Reddy <narayanreddy355@gmail.com>
2025-09-27 17:32:49 +0530

    cors added

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ import cors from "cors"
 
 const app=express()
 dotenv.config()
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(cookieParser())
 app.use(express.json())
 
